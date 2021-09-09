dedd8741cc9a83ccdff8f453f4da0bf29705afe6
xiaxia643 <xiaxia643@outlook.com>
2021-09-09 21:00:23 +0700

    update main page

--- src/App.js
+++ src/App.js
@@ -4,15 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="app">
-      <img src="" alt="" />
-      <span className="app-title">FreeCodeCamp</span>
+      <div className="app-profile">
+        <img src={process.env.PUBLIC_URL + '/profile.jpg'} alt="fcc logo" />
+        <div className="app-myname">
+          <a
+            href="https://xiaxia643.github.io"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @XiaXia
+          </a>
+        </div>
+      </div>
+      <span className="app-title">My FreeCodeCamp's Projects</span>
       <a
-        href="/solid-waffle/pages/randomQuoteMachine/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Random Quote Machine
-      </a>
+          className="app-link"
+          href="/solid-waffle/pages/randomQuoteMachine/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={process.env.PUBLIC_URL + '/quotes.png'} alt="fcc logo" width="20px" />
+          <span>Random Quote Machine</span>
+        </a>
 
     </div>
   );
