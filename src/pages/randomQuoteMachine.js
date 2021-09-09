f92e55e62a5e3297969a77f89e9c31c6e60f3fd2
xiaxia643 <xiaxia643@outlook.com>
2021-09-09 21:51:26 +0700

    add box

--- src/pages/randomQuoteMachine.js
+++ src/pages/randomQuoteMachine.js
@@ -1,11 +1,20 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './randomQuoteMachine.css';
 
 ReactDOM.render(
   <React.StrictMode>
-<div id="test">Test</div>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+function App() {
+  return <div className="appquote">
+    <div id="quote-box" className="appquote-box">
+      OK
+    </div>
+    <div id="test">Test</div>
+    iconmonstr.com
+  </div>;
+}
