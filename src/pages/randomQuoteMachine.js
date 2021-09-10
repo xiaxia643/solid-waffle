34227964d8343f341f07763637f65ebcd6e46c35
xiaxia643 <xiaxia643@outlook.com>
2021-09-10 20:17:59 +0700

    update quote

--- src/pages/randomQuoteMachine.js
+++ src/pages/randomQuoteMachine.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './randomQuoteMachine.css';
 
@@ -10,11 +10,42 @@ ReactDOM.render(
 );
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState({});
+
+  useEffect(() => {
+    const fetchQuotes = async () => {
+      const url = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+      const resp = await fetch(url);
+      const data = await resp.json();
+      setQuotes(data.quotes);
+    }
+    fetchQuotes();
+  }, []);
+
+  useEffect(() => {
+    if (quotes.length !== 0) {
+      let i = Math.floor(Math.random() * quotes.length);
+      setQuote(quotes[i]);
+    }
+  }, [quotes]);
+
   return <div className="appquote">
-    <div id="quote-box" className="appquote-box">
-      OK
+    <div id="quote-box">
+      <div id="text"><img src="iconmonstr-quote-1.svg" alt="left quote" /> {quote.quote}<img src="iconmonstr-quote-3.svg" alt="left quote" /></div>
+      <div id="author">-- {quote.author} --</div>
+      <div className="appquote-bottom">
+        <div className="appquote-bottom-left">
+          <a id="tweet-quote" href="https://twitter.com/intent/tweet"><img src="twitter.png" alt="twitter"></img></a>
+          <a id="tweet-quote" href="https://twitter.com/intent/tweet"><img src="tumblr.png" alt="twitter"></img></a>
+        </div>
+        <button id="new-quote">New quote</button>
+      </div>
     </div>
-    <div id="test">Test</div>
-    iconmonstr.com
+
+
+    <p>
+      iconmonstr.com
+    </p>
   </div>;
 }
